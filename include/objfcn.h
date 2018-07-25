a662ecb476b952b78cfbaaaa4fb794c945f26e8d
Devin Nakamura <devinn@ca.ibm.com>
2018-07-25 12:02:03 -0400

    Make main header C compatable

--- include/objfcn.h
+++ include/objfcn.h
@@ -1,14 +1,24 @@
-#ifndef OBJOPEN_HPP
-#define OBJOPEN_HPP
+#ifndef OBJFCN_H
+#define OBJFCN_H
+
 
 struct Object;
-typedef Object* ObjHandle;
+typedef struct Object* ObjHandle;
 
-// Note: flags is ignored at the moment.
-// only specified so signature is compatable with dlopen
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Note: flags is ignored at the moment.
+ only specified so signature is compatable with dlopen*/
 ObjHandle objopen(const char *file, int flags);
 
 void *objsym(ObjHandle handle, const char *sym);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
